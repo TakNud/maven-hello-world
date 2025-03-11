37d8fce669f133aed20e7d52eeb75f2a0b57d503
TakNud <almog10080@gmail.com>
2025-03-11 21:55:24 +0200

    change app.jave for testing

--- myapp/src/main/java/com/myapp/App.java
+++ myapp/src/main/java/com/myapp/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World - AlmogSofer!" );
+        System.out.println( "Thank you for sharing this project :)" );
     }
 }
